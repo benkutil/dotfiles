3a509f5b3fc91c3e829bb482c7f4dac29a67c655
Ben Kutil <thisis@benkutil.com>
2017-06-13 10:56:32 -0400

    add omf configurations

---
+++ config/omf/before.init.fish
@@ -0,0 +1,5 @@
+set fish_user_paths $fish_user_paths \
+                    ~/.homebrew/opt/gpg-agent/bin \
+                    ~/.homebrew/bin \
+                    ~/.node/bin \
+                    ~/.bin
