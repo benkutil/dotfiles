85f5957efe3aba3be57d509d3e9c4fa9ac300b13
Ben Kutil <thisis@benkutil.com>
2017-06-13 11:24:13 -0400

    add init

---
+++ config/omf/init.fish
@@ -0,0 +1,27 @@
+##
+## Environment Variables
+## - OMF
+##   - Ocean Theme
+## - Github
+##   - Github Token
+## - Homebrew
+##   - Homebrew Cask
+##
+
+
+##
+## OMF OCEAN
+##
+set -g theme_display_user yes
+set -g default_user ktb
+
+
+##
+## Homebrew
+##
+
+##
+## Homebrew Cask Options
+##
+
+set -xg HOMEBREW_CASK_OPTS "--appdir=~/Applications"
